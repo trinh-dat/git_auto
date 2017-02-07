add9df736cbf3cd883e67f66594bd5e648e10b93
Dat Trinh <quangdat212@hotmail.com>
2017-02-07 14:07:34 +0700

    remove old

--- aaa/ScrollToElement.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver import ActionChains
-from appium import webdriver
-
-
-class Scroll:
-	def scrollToElement(xpath_):
-		element = driver.find_element_by_xpath(xpath_)
-        actions = ActionChains(driver)
-        actions.move_to_element(element).perform()
